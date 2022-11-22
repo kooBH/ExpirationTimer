0bcac768485eeb83a6d43ed4fbebe1c592b0092c
KooBH <ninie4409@sogang.ac.kr>
2022-11-22 22:08:24 +0900

    update cmake

--- chrono/exp_chrono.h
+++ chrono/exp_chrono.h
@@ -1,10 +1,13 @@
+#ifndef _H_EXP_CHRONO_
+#define _H_EXP_CHRONO_
+
 #include <iostream>
 #include <chrono>
 #include <sstream>
 #include <iomanip>
 
 // https://stackoverflow.com/questions/44475868/check-expiration-date-in-c11
-bool is_expired( std::chrono::system_clock::time_point issued_time )
+inline bool is_expired( std::chrono::system_clock::time_point issued_time )
 {
   using namespace std;
   using namespace std::chrono;
@@ -12,7 +15,7 @@ bool is_expired( std::chrono::system_clock::time_point issued_time )
   return duration_cast< days >( system_clock::now() - issued_time ) > days { 1 };
 }
 
-auto operator""_issued( const char* s ) // see http://e...content-available-to-author-only...e.com/w/cpp/language/user_literal
+inline auto operator""_issued( const char* s ) // see http://e...content-available-to-author-only...e.com/w/cpp/language/user_literal
 {
   std::istringstream iss { s };
   std::tm t {};
@@ -20,3 +23,9 @@ auto operator""_issued( const char* s ) // see http://e...content-available-to-a
   return std::chrono::system_clock::from_time_t( std::mktime( &t ) );
 }
 
+/* Example
+std::cout << (is_expired( 20221020_issued ) ? "expired" : "valid") << std::endl;
+ 
+*/
+
+#endif
