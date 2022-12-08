60d5a0a405842c12c47916030a3e1f67fb292dd2
KooBH <ninie4409@sogang.ac.kr>
2022-12-08 16:29:39 +0900

    alt

--- chrono/exp_chrono.h
+++ chrono/exp_chrono.h
@@ -12,11 +12,11 @@ inline bool is_expired( std::chrono::system_clock::time_point issued_time )
   using namespace std;
   using namespace std::chrono;
   typedef duration< int, ratio_multiply < hours::period, ratio<24> >::type > days;
+
   return duration_cast< days >( system_clock::now() - issued_time ) > days { 1 };
 }
-
-inline auto operator""_issued( const char* s ) // see http://e...content-available-to-author-only...e.com/w/cpp/language/user_literal
-{
+inline auto operator""_issued(const char* s){
+//inline  std::chrono::time_point<std::chrono::system_clock> issued(const char* s){
   std::istringstream iss { s };
   std::tm t {};
   iss >> std::get_time( &t, "%Y%m%d" );
